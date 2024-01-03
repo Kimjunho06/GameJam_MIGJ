2daea23face5c02f3f27809e57c8c8709c6bb935
KK <kangjh2006@gmail.com>
2024-01-03 18:52:18 +0900

    SoundSlider

---
+++ Assets/01.Scripts/Test/JsonLoad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    public float bgmVolume;
+    public float sfxVolume;
+}
+
+public class JsonLoad : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
