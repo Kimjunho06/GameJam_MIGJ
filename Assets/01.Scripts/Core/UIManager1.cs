c0ef27734dbaf776ab3352e10b4067634d721ac9
KK <kangjh2006@gmail.com>
2024-01-03 23:37:36 +0900

    Intro Fix

---
+++ Assets/01.Scripts/Core/UIManager1.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+public class UIManager1 : MonoBehaviour
+{
+    [SerializeField] private GameObject _fadeImage;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeTime;
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button optionButton;
+    [SerializeField] private Button exitButton;
+
+    private bool isFaded = false;
+
+    private Button selectedButton;
+
+    private void Awake()
+    {
+        selectedButton = startButton;
+    }
+
+    private void Update()
+    {
+        selectedButton.Select();
+
+        if(selectedButton == startButton && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedButton = exitButton;
+        }
+        if (selectedButton == exitButton && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedButton = startButton;
+        }
+    }
+
+    public void OnClickStart()
+    {
+        _fadeImage.SetActive(true);
+        if (isFaded == false)
+        {
+            isFaded = true;
+            fadeImage.DOFade(1, fadeTime).OnComplete(()=> isFaded = false);
+        }
+        Invoke("ChangeMain", fadeTime);
+    }
+
+    public void ChangeMain()
+    {
+        SceneManager.LoadScene("SampleSceneJiheon");
+    }
+
+    public void ChangeIntro()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    public void ChangeOption()
+    {
+        SceneManager.LoadScene("InGameOption");
+    }
+
+
+    public void OnClickOption()
+    {
+        _fadeImage.SetActive(true);
+        if (isFaded == false)
+        {
+            isFaded = true;
+            fadeImage.DOFade(1, fadeTime);
+            isFaded = false;
+        }
+        Invoke("ChangeOption", fadeTime);
+    }
+
+    public void RealNewGame()
+    {
+        _fadeImage.SetActive(true);
+        if (isFaded == false)
+        {
+            isFaded = true;
+            fadeImage.DOFade(1, fadeTime).OnComplete(() => isFaded = false);
+        }
+        Invoke("ChangeIntro", fadeTime);
+    }
+    
+    public void RealExitGame()
+    {
+        Application.Quit();
+    }
+}
