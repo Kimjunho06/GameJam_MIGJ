5c412c5248287a8c9d3f6aec785284ab230492e7
김준호 <kjhlove0119@naver.com>
2024-01-05 00:53:52 +0900

    d

--- Assets/01.Scripts/CleanArea/CleanArea.cs
+++ Assets/01.Scripts/CleanArea/CleanArea.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CleanArea : MonoBehaviour
 {
     public int maxCnt;
     [SerializeField] private ObjectType objectType;
     [SerializeField] private List<GameObject> objects;
-    
+
+    [SerializeField] private Image objImg;
+    [SerializeField] private Sprite objSprite;
+    [SerializeField] private TextMeshProUGUI objCntTxt;
+
     private int cnt;
 
     public bool isClear;
@@ -28,12 +34,16 @@ public class CleanArea : MonoBehaviour
 
         objects[cnt].SetActive(true);
         cnt++;
+
+        objImg.sprite = objSprite;
     }
 
     private void Update()
     {
         if (cnt == maxCnt + 1)
             isClear = true;
+
+        objCntTxt.SetText($"{cnt - 1} / {maxCnt}");
     }
 
     private void OnTriggerEnter(Collider other)
